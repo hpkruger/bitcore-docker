6139e71cefd287baf301be2069993accf6aa04db
slush0 <slush@satoshilabs.com>
2016-07-04 15:06:45 +0200

    Updated bash console

--- bash.sh
+++ bash.sh
@@ -5,4 +5,12 @@ docker run -t --cidfile=bitcore.cid \
         -p 3001:3001 \
         -i bitcore /bin/bash
 
+read -p "Store changed data to image? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # do dangerous stuff
+    docker commit `cat bitcore.cid` bitcore
+fi
+
 rm bitcore.cid
